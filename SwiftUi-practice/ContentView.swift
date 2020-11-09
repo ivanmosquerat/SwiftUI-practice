65a7c37869327b8ddb4606b47a694e0a6af515c9
Ivan Mosquera <ivanmosquerat13@gmail.com>
2020-11-10 00:20:26 +1100

    Title section created.

--- SwiftUi-practice/ContentView.swift
+++ SwiftUi-practice/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            
+            Text("Turtle rock")
+                .font(.title)
+            
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
