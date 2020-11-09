ee3a8e1f006737349b769ed291ec538a6bc44bdf
Ivan Mosquera <ivanmosquerat13@gmail.com>
2020-11-09 23:27:32 +1100

    Initial Commit

---
+++ SwiftUi-practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi-practice
+//
+//  Created by Ivan Mosquera on 9/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
