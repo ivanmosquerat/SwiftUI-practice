2e80e37097e087840f1e929dc98b5142c76a13af
Ivan Mosquera <ivanmosquerat13@gmail.com>
2020-12-29 23:39:12 +1100

    Badges created. App icon added.

---
+++ SwiftUi-practice/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUi-practice
+//
+//  Created by Ivan Mosquera on 29/12/20.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
